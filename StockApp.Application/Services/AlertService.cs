d58c8c2d5a080a6cbaeca94ac9be1fcd705a1479
clarachence <domitilachence@gmail.com>
2025-06-29 16:18:01 -0300

    Alterações nos testes

--- StockApp.Application/Services/AlertService.cs
+++ StockApp.Application/Services/AlertService.cs
@@ -1,13 +1,23 @@
 ﻿using System.Threading.Tasks;
-using StockApp.Application.Interfaces.Services;
+using StockApp.Application.Interfaces.Services; 
+using Microsoft.Extensions.Logging; 
 
 namespace StockApp.Application.Services
 {
     public class AlertService : IAlertService
     {
+        private readonly ILogger<AlertService> _logger; 
+
+        public AlertService(ILogger<AlertService> logger) 
+        {
+            _logger = logger;
+        }
+
         public async Task SendAlertAsync(string userId, string message)
         {
-            await Task.CompletedTask;
+           
+            _logger.LogInformation($"Alerta enviado para o usuário {userId}: {message}");
+            await Task.CompletedTask; 
         }
     }
 }
--- StockApp.Domain.Test/Services/AlertServicesTests.cs
+++ StockApp.Domain.Test/Services/AlertServicesTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿
+using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using Microsoft.Extensions.Logging;
 using StockApp.Application.Services;
 
 namespace StockApp.Domain.Test.Services
@@ -10,11 +13,14 @@ namespace StockApp.Domain.Test.Services
         public async Task SendAlertAsync_Should_NotThrowException()
         {
             
-            var service = new AlertService();
+            var mockLogger = new Mock<ILogger<AlertService>>();
+
+            var service = new AlertService(mockLogger.Object);
 
             var exception = await Record.ExceptionAsync(() => service.SendAlertAsync("user123", "Mensagem de teste"));
 
             Assert.Null(exception);
+
         }
     }
 }
