c4b49641445860087776e3c037b2621268856798
IsabelaNeves1 <nevesisabela03@gmail.com>
2025-06-30 07:24:43 -0300

    feat: integrar análise de sentimento às avaliações de produtos (reviews)

---
+++ StockApp.Application/DTOs/ReviewDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StockApp.Application.DTOs
+{
+    public class ReviewDTO
+    {
+        public string UserId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
--- StockApp.Application/Services/ReviewService.cs
+++ StockApp.Application/Services/ReviewService.cs
@@ -6,20 +6,24 @@ namespace StockApp.Application.Services
     public class ReviewService
     {
         private readonly IReviewRepository _reviewRepository;
+        private readonly ISentimentAnalysisService _sentimentService;
 
-        public ReviewService(IReviewRepository reviewRepository)
+        public ReviewService(IReviewRepository reviewRepository, ISentimentAnalysisService sentimentService)
         {
             _reviewRepository = reviewRepository;
+            _sentimentService = sentimentService;
         }
 
         public async Task AddReviewAsync(int productId, string userId, int rating, string comment)
         {
+            var sentiment = _sentimentService.AnalyzeSentiment(comment);
             var review = new Review
             {
                 ProductId = productId,
                 UserId = userId,
                 Rating = rating,
                 Comment = comment,
+                Sentiment = sentiment,
                 Date = DateTime.UtcNow
             };
 
